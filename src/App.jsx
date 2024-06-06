4945f0b6ed10557334382bd872680da4797b016b
Jonatha Lima <jonatha.lima14@gmail.com>
2024-06-06 15:33:46 -0300

    adicionar o componente Item

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,103 @@
 import './App.css';
 import OrderDetails from './components/OrderDetails';
+import Item from './components/Item';
+import { useState } from 'react';
 
 function App() {
 
-    const items = [
-        {
-            
-            id: 1, 
-            photo: "real_madrid.webp",
-            name: "Real Madrid",
-            price: 119.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 2, 
-            photo: "milan.png",
-            name: "Milan",
-            price: 99.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 3, 
-            photo: "chelsea.webp",
-            name: "Chelsea",
-            price: 99.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 4, 
-            photo: "barcelona.png",
-            name: "Barcelona",
-            price: 109.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 5, photo: "benfica.png",
-            name: "Benfica",
-            price: 89.49,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 6, 
-            photo: "manchester.webp",
-            name: "Manchester City",
-            price: 129.79,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 7, 
-            photo: "bayern.webp",
-            name: "Bayern",
-            price: 119.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 8, 
-            photo: "psg.png",
-            name: "PSG",
-            price: 94.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 9, 
-            photo: "ajax.webp",
-            name: "Ajax",
-            price: 89.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        }
-    ];
+    const [items, setItems] = useState(
+        [
+            {
+                
+                id: 1, 
+                photo: "real_madrid.webp",
+                name: "Real Madrid",
+                price: 119.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 2, 
+                photo: "milan.png",
+                name: "Milan",
+                price: 99.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 3, 
+                photo: "chelsea.webp",
+                name: "Chelsea",
+                price: 99.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 4, 
+                photo: "barcelona.png",
+                name: "Barcelona",
+                price: 109.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 5, photo: "benfica.png",
+                name: "Benfica",
+                price: 89.49,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 6, 
+                photo: "manchester.webp",
+                name: "Manchester City",
+                price: 129.79,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 7, 
+                photo: "bayern.webp",
+                name: "Bayern",
+                price: 119.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 8, 
+                photo: "psg.png",
+                name: "PSG",
+                price: 94.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 9, 
+                photo: "ajax.webp",
+                name: "Ajax",
+                price: 89.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            }
+        ]
+    );
 
     const itemsInBag = items.filter(item => item.isInBag);
+
+    function selectHandler(id) {
+        let selectedItem = items.filter(item => item.id === id)[0];
+        selectedItem.isInBag = !selectedItem.isInBag;
+        setItems(items.map(item => item.id === id ? selectedItem : item));
+    }
 
     return ( 
         <>
@@ -95,25 +105,11 @@ function App() {
                 <h4>Jersey Shop Made with React JS</h4>
 
                 { items.map(item => 
-                    <div key={item.id} className={`product ${ item.isInBag ? 'selected' : '' }`}>
-                        <div className="photo">
-                            <img src={"./img/" + item.photo} />
-                        </div>
-                        <div className="description">
-                            <span className="name">{item.name}</span>
-                            <span className="price">$ {item.price}</span>
-
-                            {
-                                item.isInBag &&
-                                <div className="quantity-area">
-                                    <button>-</button>
-                                    <span className="quantity">1</span>
-                                    <button>+</button>
-                                </div>
-                            }
-                            
-                        </div>
-                    </div>
+                    <Item 
+                        selectProduct={(id) => selectHandler(id)}
+                        key={item.id} 
+                        item={item} 
+                    />
                 ) }
             </section>
 
